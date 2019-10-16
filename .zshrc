fcd8d8473b175cbcd44e8055d85f3dc69f1f732c
Carl Beekhuizen <carl.beek@gmail.com>
2019-10-16 11:05:28 +0200

    update for zsh and cleanup

---
+++ .zshrc
@@ -0,0 +1,18 @@
+plugins=(
+    brew
+    cask
+    git
+    jsontools
+    osx
+    pep8
+    pip
+    pyenv
+    python
+    virtualenv
+    vscode
+    z
+)
+
+ZSH_THEME="spaceship"
+
+source ~/.aliases
--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Install command-line tools using Homebrew.
 
@@ -8,42 +8,76 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-if [ -z `grep "/usr/local/bin/bash"  /etc/shells` ]; then
-  echo "/usr/local/bin/bash" >> /etc/shells
-fi
-sudo chsh -s /usr/local/bin/bash
-brew install bash-completion2
+echo "  🍺 installing " $CASKS
+CASKS=(
+    vscodium  # visual-studio-code built from source
+    sublime-text
+    kitty  # gpu accelerated terminal
+    
+    brave-browser
+    google-chrome
+    firefox
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+    zoomus
+    telegram
+    slack
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+    alfred
+    transmission
+    vlc
+)
+brew cask install $CASKS
 
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install homebrew/php/php56 --with-gmp
+echo "  🍺 installing " $BREWS
+BREWS=(
+    git
+    git-lfs
+    gnupg
+    grep
+    wget
+    openssh
+    screen
+    vim --with-override-system-vi
+    p7zip
+    zopfli
+    htop-osx
+    unrar
+    node
+    sshfs
+    pyenv
+    pyenv-virtualenv
 
+    coreutils
+    moreutils
+    findutils
+    docker
+    ffmpeg
+    tree
+    youtube-dl
+
+    koekeishiya/formulae/skhd
+    crisidev/homebrew-chunkwm/chunkwm
+)
+brew install $BREWS
+
+
+echo "  🍺 installing fonts"
+FONTS=(
+  font-anonymous-pro
+  font-dejavu-sans-mono-for-powerline
+  font-droid-sans-mono-for-powerline
+  font-meslo-lg font-input
+  font-inconsolata font-inconsolata-for-powerline
+  font-liberation-mono-for-powerline
+  font-liberation-sans
+  font-meslo-lg
+  font-nixie-one
+  font-office-code-pro
+  font-raleway font-roboto
+  font-source-code-pro font-source-code-pro-for-powerline
+  font-source-sans-pro
+)
 # Install font tools.
 brew tap bramstein/webfonttools
 brew install sfnt2woff
@@ -51,80 +85,26 @@ brew install sfnt2woff-zopfli
 brew install woff2
 # Instal some more fonts:
 brew tap caskroom/fonts
-brew cask install \
-  font-anonymous-pro \
-  font-dejavu-sans-mono-for-powerline \
-  font-droid-sans-mono-for-powerline \
-  font-meslo-lg font-input \
-  font-inconsolata font-inconsolata-for-powerline \
-  font-liberation-mono-for-powerline \
-  font-liberation-sans \
-  font-meslo-lg \
-  font-nixie-one \
-  font-office-code-pro \
-  font-raleway font-roboto \
-  font-source-code-pro font-source-code-pro-for-powerline \
-  font-source-sans-pro
+brew cask install $FONTS
 
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# github.com/sindresorhus/quick-look-plugins
+QUICKLOOKS=(
+    # quicklookase
+    qlcolorcode
+    qlimagesize
+    qlmarkdown
+    qlstephen
+    qlvideo
+    quicklook-csv
+    quicklook-json
+    webpquicklook
+)
+caskin $QUICKLOOKS
+# restart quicklook manager
+qlmanage -r
+brew cleanup
 
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
-brew install htop-osx
-brew install mongodb
-brew install node
-brew install unrar
-brew install sshfs
-
-# Install all them tasty window manger stuffies:
-brew install koekeishiya/formulae/skhd
-brew tap crisidev/homebrew-chunkwm
-brew install chunkwm
-
-# Quicklook plugins:
-brew cask install \
-	qlcolorcode qlmarkdown qlprettypatch qlstephen \
-	qlimagesize \
-	quicklook-csv quicklook-json epubquicklook
 
 # Remove outdated versions from the cellar.
 brew cleanup
